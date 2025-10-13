82774b7ac6a6a5d09a4ae50e76fbb63c80523b9a
JereAntunez <jeremias.antunez18@gmail.com>
2025-10-13 14:30:47 -0300

    Initial commit

---
+++ src/test/java/com/github/grupo_s/nyx_app/NyxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.grupo_s.nyx_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
