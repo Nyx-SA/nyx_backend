6d573af8ed5da020f01ae6a4bce4e83d6c4ac5a2
JereAntunez <jeremias.antunez18@gmail.com>
2025-10-14 19:27:47 -0300

    feat:user implemented with basic features

---
+++ src/main/java/com/github/grupo_s/nyx_app/users/User.java
@@ -0,0 +1,32 @@
+package com.github.grupo_s.nyx_app.users;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String username;
+
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
